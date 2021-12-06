09c29dd8b296fe059a7e95420bf07a7ed0c00374
Alexjandro Mendoza <alexjandromendoza@macbook-pro-2.lan>
2021-12-06 00:04:33 -0500

    Form works fine, displays message indicating it is received, Contact part finished

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,20 @@ export default class About extends Component {
             <div>
                 <h2>About</h2>
                 <p> Hello, my name is Alexjandro Mendoza and I'm currently a student pursuing a bachelors degree in Computer Science. 
-                    I have a passion for investing and would like to take my earnings from
-                    my career job to spend on rental properties. I wish I had a mentor in the real estate field but 
-                    it seems like I will have to make mistakes on my own and network to meet people.
-                    I live in Yonkers and it sucks! However, I am grateful for having a roof over my head. 
-                    I'm into video games, food, technology, and I loved speeding on the highway 
-                    but these tickets are not fun so my speeding days are over. Check out my highlights for some videos games in the highlights section! If you would like to
-                    my resume, download it below 😁
+                <br/>I have a passion for investing and would like to take my earnings from
+                 my career job to spend on rental properties. 
+                 <br/>I wish I had a mentor in the real estate field but 
+                it seems like I will have to make mistakes on my own and network to meet people.
+                <br/>I live in Yonkers and it sucks! However, I am grateful for having a roof over my head. 
+                <br/> I'm into video games, food, technology, and I loved speeding on the highway 
+                 but these tickets are not fun so my speeding days are over. 
+                 <br/>Check out my highlights for some videos games in the highlights section!
+                 <br/>If you would like to my resume, download it below 😁
                     <p>-AM</p>
 
-                    <small>hey</small>
+                    <small>resume here</small>
+
+                    
                      
                 </p>
             </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {useForm} from "react-hook-form";
 
 export default class Contact extends Component {
+
+
+
+   /* const {register, handleSubmit, errors} = useForm(); 
+    const onSubmit = data =>{console.log(data)};*/
+
+    onButtonClickHandler = () => {
+        window.alert("Message received! I'll get back to you when I can! 😁")
+      };
+
+
+
     render() {
+
         return (
             <div>
                 <h1>For questions, comments, or concerns</h1>
-                <p>Primary #: 914-999-9999</p>
-                <p>Email: 4lexmendezz@gmail.com</p>
+                <p>Primary #: 914-999-9999
+                <br/>
+                Email: 4lexmendezz@gmail.com
+                </p>
                 <h5>Please fill out the form below for me to get back to you</h5>
 
+                <form>
+                    <input type="text" placeholder="Full Name" name="name" />
+                    <br/>
+                    <br/>
+                    <input type="text" placeholder="Email" name="email" />
+                    <br/>
+                    <br/>
+                    <input class= "pow" maxlength="125" size="100" type="text" placeholder="Short Message" name="message" />
+                    <br/>
+                    <br/>
+                    <button onClick={this.onButtonClickHandler}>Submit</button>
+                    
                 
+                </form>
+    
             </div>
+
+           
+
+
+
+
+
         )
     }
 }
