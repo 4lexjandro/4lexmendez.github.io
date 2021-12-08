2c6398165b4de5ca4897cb96aa0c59d3199564dc
Alexjandro Mendoza <alexjandromendoza@macbook-pro.lan>
2021-12-08 01:41:31 -0500

    transition is giving errors, the goal is to have a box pop in and out whenever you click it at all times

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
 import { useSpring, animated, useTransition } from 'react-spring';
+import { useState } from 'react';
 
 
 
@@ -12,38 +13,54 @@ import { useSpring, animated, useTransition } from 'react-spring';
 
 function App() {
 
+  const [slideIn, setSlideIn] = useState(false)
+
   const style1 = useSpring({
     from: {opacity: 0, marginTop: -100},
     to: { opacity: 1, marginTop: 0},
     config: {duration: 3000 }
 })
 
-const style2 = useSpring({
+  const style2 = useSpring({
   from: { marginLeft: 0},
   to: { marginLeft: 900 },
   config: {duration: 3000 }
 })
 
+  const transition = useTransition(slideIn, {
+  from: { x: -200 , y: 0},
+  enter: { x: 0 , y: 0 },
+  leave: {x: -200 , y: 0 }
+})
 
+const moveMe = () => {
+  setSlideIn(!slideIn)
+
+}
 
 
 
   return (
 <div className="App">
-  
-
       <animated.div style={style1}>
         <div className="topDiv">
         <small><NavbarComp/></small>
           <animated.div style={style2}>
 
-            
-            
-
             <h1>Welcome</h1>
             </animated.div>
         </div>
        </animated.div>
+       <button onClick={() => moveMe()}>Move Me</button>
+       <div>
+         {transition((style, item) => 
+           item ?
+           <animated.div style={style}>
+             <div className="div2">Hey hey</div>
+           </animated.div>
+           : ''
+      )}
+       </div>
                                
       
 
