af77bc1e2703fc0ae6a7e7680cd4a154b771018b
Alexjandro Mendoza <alexjandromendoza@macbook-pro.myfiosgateway.com>
2021-12-08 22:37:11 -0500

    cropped images to desired goal, now to organize them and add details and links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,12 +16,15 @@ export default class Projects extends Component {
             <div>
                <h2> Projects incoming</h2>
 
-               <img src={calc} alt="calc image" height={350} width={350}/>
+               <img src={calc} alt="calc image" height={250} width={350}/>
                <p></p>
-               <img src={designPad} alt="designPad image" height={350} width={350}/>
+               <small>In this project, I used javascript, css, and html to create a functional calculator</small>
+               <img src={designPad} alt="designPad image" height={250} width={350}/>
                <p></p>
-               <img src={favLinks} alt="favLinks image" height={350} width={350}/>
+               <small></small>
+               <img src={favLinks} alt="favLinks image" height={250} width={350}/>
                <p></p>
+               <small></small>
 
 
 
